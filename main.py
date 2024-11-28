b796d3e98c37a03cc58485fa4084298623390222
Leonardo D <8dominguezleo@gmail.com>
2024-11-28 18:28:06 +0100

    Edit headless mode

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def update_script_status(data):
 def get_aena_data(airports=AIRPORT, TIMEZONE=TIMEZONE, HISTORY_DAYS=HISTORY_DAYS):
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    #options.add_argument('--headless')  # Evita que se abra el navegador
+    options.add_argument('--headless')  # Evita que se abra el navegador
     options.add_argument('--window-size=1920,1080') # Tamaño de la ventana
     options.add_argument('--disable-extensions')  # Deshabilitar extensiones
     options.add_argument('--disable-dev-shm-usage')  # Deshabilitar el uso compartido de memoria
