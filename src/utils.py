cfeee70e881c8ebe1d70fc1f78a20e2cf271201f
Leonardo D <8dominguezleo@gmail.com>
2025-02-18 10:44:39 +0100

    Fix date suffix

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,11 @@ from datetime import datetime, timedelta
 def get_future_date_label(days_in_future):
     current_date = datetime.now()
     future_date = current_date + timedelta(days=days_in_future)
-    return future_date.strftime("Choose %A, %B {day}th, %Y").format(day=future_date.day)
+    day_suffix = get_day_suffix(future_date.day)
+    return future_date.strftime(f"Choose %A, %B {future_date.day}{day_suffix}, %Y")
+
+def get_day_suffix(day):
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        return "th"
+    else:
+        return ["st", "nd", "rd"][day % 10 - 1]
